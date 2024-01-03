f1a6d60d7d9642c7fb7ca204a53e96440259227e
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-02 18:57:44 -0600

    Acondicionando factories

---
+++ database/factories/ProfileFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Profile;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profile>
+ */
+class ProfileFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'identifier' => "PU-" . Str::padLeft(Profile::count(), 5, '0'),
+            'user_id' => fake()->numberBetween(1, 5),
+        ];
+    }
+}
