0c238ea59529ab2963a2f2ea70f7371ca208c8f2
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-16 19:32:40 -0600

    Desplegando comentarios

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,12 @@
 <x-layout title="Posts">
     <div class="flex flex-col justify-center items-center w-full h-screen">
         @foreach ($posts as $post)
-            <span class="text-2xl font-bold text-blue-500">{{$post->title}}</span>
+            <div class="border-2 border-blue-200 mb-4 p-4 rounded-xl">
+                <span class="block text-2xl font-bold text-blue-500">{{$post->title}}</span>
+                @foreach ($post->comments as $comment)
+                    <span class="block text-red-500 text-xs">{{$comment->comment}}</span>
+                @endforeach
+            </div>
         @endforeach
     </div>
 </x-layout>
