fb623d4bac28096718d3341fbec6a9bb7adb32fc
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-16 20:12:07 -0600

    Facotories

--- database/factories/StudetCourseFactory.php
+++ database/factories/StudetCourseFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class CourseFactory extends Factory
+class StudetCourseFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,7 +17,8 @@ class CourseFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->words(5),
+            'student_id' => fake()->numberBetween(1, 5),
+            'course_id' => fake()->numberBetween(1, 5),
         ];
     }
 }
