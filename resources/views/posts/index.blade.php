a4aae37850a4ebfb4be1e7c42a3df18c2d29c886
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-01-16 19:46:04 -0600

    Vista de commentarios agregada

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,7 @@
 <x-layout title="Posts">
+    <div class="mt-4 mr-4 flex justify-end">
+        <a href="{{route('comments.index')}}" class="bg-blue-500 text-white font-semibold py-2 px-4 hover:bg-blue-400">Vista de comentarios -></a>
+    </div>
     <div class="flex flex-col justify-center items-center w-full h-screen">
         @foreach ($posts as $post)
             <div class="border-2 border-blue-200 mb-4 p-4 rounded-xl">
